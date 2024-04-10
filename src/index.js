066d62ad02b7c1969035593dabb124a009354740
Uche <nnajiuchejosiah@gmail.com>
2024-04-10 23:43:36 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  HashRouter as Router,
+  Link,
+  Outlet,
+ 
+  createRoutesFromElements,
+} from "react-router-dom";
+import News from "./routes/News";
+import People from "./routes/People";
+import UpcomingEvents from "./routes/UpcomingEvents";
+import PublishedArticle from "./routes/PublishedArticle";
+import Workshops from "./routes/Workshops";
+import Home from "./routes/Home";
+
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path="News" element={<News />} />
+        <Route path="People" element={<People />} />
+        <Route path="PublishedArticle" element={<PublishedArticle />} />
+        <Route path="UpcomingEvents" element={<UpcomingEvents />} />
+        <Route path="Workshops" element={<Workshops />} />
+      </Route>
+    </Routes>
+  </Router>
+);
+
+createRoot(document.getElementById("root")).render(<App />);
+
---
+++ src/routes/PublishedArticle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function PublishedArticle(){
+
+    return (
+        <div className="central">
+
+        <h1>PublishedArticle</h1>
+        </div>
+
+    );
+
+
+}
+
+
+export default PublishedArticle;
