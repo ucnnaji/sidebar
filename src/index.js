c0791d1e63c0c174cce5ab79b36417ce2d70c415
Uche <nnajiuchejosiah@gmail.com>
2024-04-29 14:29:20 +0100

    Another trial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-import "./App.css";
-import Navbar from "./components/Navbar";
-=======
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -16,40 +12,25 @@ import {
  
   createRoutesFromElements,
 } from "react-router-dom";
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
 import News from "./routes/News";
 import People from "./routes/People";
 import UpcomingEvents from "./routes/UpcomingEvents";
 import PublishedArticle from "./routes/PublishedArticle";
 import Workshops from "./routes/Workshops";
 import Home from "./routes/Home";
-
-<<<<<<< HEAD
-
-=======
 import Navbar from "./components/Navbar";
 import "./App.css";
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
-
 
 const AppLayout = () => (
   <>
-<<<<<<< HEAD
+
     <div>
-      
     <Navbar />
     </div>
     
     <Outlet />
-   
-=======
-    <Navbar />
-    <Outlet />
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
+
   </>
 );
 
--- src/routes/PublishedArticle.js
+++ src/routes/PublishedArticle.js
@@ -1,14 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import Content from "../Content.js";
 import "../App.css";
-=======
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
+
 
 function PublishedArticle(){
 
     return (
-<<<<<<< HEAD
         <>
         
         <div className="central">
@@ -16,19 +14,14 @@ function PublishedArticle(){
         </div>
          
 
-        <>
-            <Content />
-        </>
-
-        </>
-
         
-=======
-        <div className="central">
+            <Content />
+       
 
-        <h1>PublishedArticle</h1>
-        </div>
->>>>>>> c8cca45fc2d1facda37cca0195732bb8dde07c01
+    
+
+        </>
+
 
     );
 
